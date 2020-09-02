3c95d3115fa90f53f608c7bec1ad65e27f6ae93b
DMVance <csljrrt@gmail.com>
2020-09-02 02:16:56 -0500

    edit comments for accuracy

--- alpha_sort_4.1.py
+++ alpha_sort_4.1.py
@@ -3,34 +3,32 @@
 # letters, counts occurrences of each letter, sorts alphabetically and prints only highest number of occurrences
 # of each letter in any single word. Sample input: apple banana orange  Sample output: a(3), b, e, g, l, n(2), o, p(2), r
 
-import collections # modules in the standard python library 
-alphabet = list("abcdefghijklmnopqrstuvwxyz")
+import collections, string # modules in the standard python library 
 
 # Get the word(s) from the user
 print("Please enter the words and press [Enter]: ")
 wrdset = input("--> ").lower()
-#print(wrdset)
 
 # split the string of words into individual words and eliiminate unnecessary characters
-words = wrdset.replace("-", " ").replace(",", " ").replace("*", " ").split(" ") # delimiter = any character in wrdset that isn't in alphabet
-##print(words)
+words = wrdset.replace("-", " ").replace(",", " ").replace("*", " ").split() # delimiter = any character in wrdset that isn't in alphabet
 
 # iterate over each word's letters and count letter occurrences, then add highest number of occurrences of each letter to new dictionary
+alphabet = string.ascii_lowercase # provides the lowercase alphabet
 final_dict = dict()
 for w in words:
-    alpha = [l for l in w if l in alphabet] # uses list comprehension to filter out any character not in the alphabet, including spaces https://realpython.com/list-comprehension-python/
-    count_dict = dict(collections.Counter(alpha)) # provide the number of occurrences of each letter in each word and arrange alphabetically
+    alpha = [l for l in w if l in alphabet] # uses list comprehension to filter out any character not in the alphabet, including spaces
+    count_dict = dict(collections.Counter(alpha)) # provide the number of occurrences of each letter in each word
     
-    for k, v in sorted(count_dict.items()): # send highest letter count for each letter into new dictionary, d
+    for k, v in sorted(count_dict.items()): # send highest letter count for each letter into new dictionary, final_dict, and arrange alphabetically
         if k not in final_dict or final_dict[k] < count_dict[k]:
             final_dict[k] = count_dict[k]
 
-# Print with this format: a(3), b, c, d(2),...
+# Format like this: a(3), b, c, d(2),...
 strung_out = ""
 for k,v in sorted(final_dict.items()):
     if final_dict[k] != 1:
-        strung_out = strung_out + str(k) + "(" + str(final_dict[k]) + "), "
+        strung_out = f"{strung_out}{str(k)}({str(final_dict[k])}), "
     else:
-        strung_out = strung_out + str(k) + ", "
+        strung_out = f"{strung_out}{str(k)}, "
 
-print(strung_out[:-2]) # print final output - eliminate the comma and space after the last element
+print(strung_out[:-2]) # print final output and eliminate the comma and space after the last element
